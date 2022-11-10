6dc640f8171c6e1835ebb8f3237d89483ffa71a8
datahawk <claysonwillis@gmail.com>
2022-11-10 13:36:45 +0000

    configuring dynamic nfs provisioner 11-10-2022

--- dynamic-nfs-provisioner/dynamic-nfs.sh
+++ dynamic-nfs-provisioner/dynamic-nfs.sh
@@ -1,7 +1,16 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-    --set nfs.server=192.168.50.201 \
+    --set nfs.server=nfs.cmdcenter.local \
     --set nfs.path=/K8sVolumes/olvm/ubsrv
+
+helm install nfs-storage nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=nfs.cmdcenter.local \
+    --set nfs.path=/exported/path
 
 # Set Storage Class to Default
 kubectl patch storageclass nfs-client -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+
+nfs.cmdcenter.local  192.168.50.201
+
+echo 'nfs.cmdcenter.local  192.168.50.201' | sudo tee -a /etc/hosts
